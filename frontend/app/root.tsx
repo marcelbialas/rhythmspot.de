d4155a7fec68dbe39d4cc7727ab43a295ec21b6d
Marcel Bialas <marcelbialas@gmail.com>
2024-08-01 18:30:34 +0200

    added shadcn/ui to project

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -5,11 +5,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import "./tailwind.css";
+import "./globals.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="de">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
